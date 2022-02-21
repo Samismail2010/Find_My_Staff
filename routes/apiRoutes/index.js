f0199d3f7d3bd2e74a25737846908ff7486d4483
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-02-21 02:07:22 -0600

    prompt section

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,52 @@
 const inquirer = require('inquirer');
+const express = require('express');
+const db = require('../../db/connection');
+const consoleTable = require('console.table');
+const mysql = require('mysql2');
+
+//const viewDept
+//const viewRole
+//const viewEmployee
 
 
-const questions = [
+const promptUser = () => {
+    inquirer.prompt ([
     {
+        type: 'list',
+        name: 'choice',
+        message: 'What would you like to do?',
+        choices: [ 
+                    'View All Departments',
+                    'View All Roles',
+                    'View All Employees',
+                    'Close App'
+        ]
+    }
+])
+
+.then((answers) => {
+    const {choices} = answers;
+
+    if (choices === 'View All Departments') {
+        viewDept();
 
     }
-];
 
-const init = () => {
-    inquirer.prompt(questions)
+    if (choices === 'View All Roles') {
+        viewRole();
+    }
+
+    if (choices === 'View All Employees') {
+        viewEmployee();
+    }
+
+    if (choices === 'Close App'){
+        console.log('Connection Ended')
+        db.end();
+    }
+
+    })
 }
-init();
+
+    
+module.exports = promptUser();
