bb9b8c41b576fb256d74016671afc4b602712f1b
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-05-01 10:23:25 -0500

    redo files

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const inquirer = require('inquirer');
+const db = require('');
+const consoleTable = require('console.table');
+const mysql = require('mysql2');
+
+
